c9f60be78daae7c78aa34287448b271a0d5e08c5
jaschamerle <jascha.merle@gmail.com>
2024-07-28 01:57:13 +0200

    feat( app.py ): initial code for main app

--- ui/app.py
+++ ui/app.py
@@ -1,3 +1,30 @@
+from tkinter import Tk
+from threading import Thread
 
 class App:
-    pass
+
+    __window: Tk
+    __main_thread: Thread
+    __run: bool
+
+    def __init__(self):
+        self.__window = Tk("Name")
+        self.__main_thread = Thread(target=self.interface)
+        self.__run = False
+
+        self.load_ui()
+
+    def interface(self):
+        while self.__run:
+            self.__window.update()
+
+    def start(self):
+        self.__run = True
+        self.__main_thread.start()
+
+    def stop(self):
+        self.__run = False
+        self.__main_thread = Thread(target=self.interface)
+
+    def load_ui(self):
+        pass
